8a4b55019551e471eca27d58278b0124b86a48bc
Ayman <aabdellatief@IT-Blocks.com>
2026-06-14 13:57:08 +0300

    feat: add a basic data layer to the API

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using System.ComponentModel;
+using System.Data;
+using Npgsql;
+using CommandAPI.Data; // Or whatever namespace you gave the AppDbContext
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = new NpgsqlConnectionStringBuilder
+{    
+  ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection"),    
+  Username = builder.Configuration["DbUserId"],    
+  Password = builder.Configuration["DbPassword"]
+};
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>    
+  options.UseNpgsql(connectionString.ConnectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
